67666df13d282c33e06b1c8471f3cf66004fe685
Matheus Marques <matheusmarujo26@gmail.com>
2024-10-25 16:00:29 -0300

    Add files via upload

---
+++ info_extrair_dados_06.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# dados_html = pd.read_html('https://en.wikipedia.org/wiki/AFI%27s_100_Years...100_Movies')
+# print(len(dados_html)) # ver os gráficos
+# top_filmes = dados_html[1] # selecionar a tabela
+# top_filmes.to_html('top_filmes.html')
+
+# dados_imdb = pd.read_xml('imdb_top_1000.xml')
+# print(dados_imdb.head(3)) # mostrar os primeiros registros
